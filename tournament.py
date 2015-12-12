e7daba4f767b80af0b5bae0b96f93fdd1357c4a4
embersyc <embersyc@gmail.com>
2015-12-12 13:28:33 -0500

    feat: Finalizing project
    All tests now pass.
    Modified tests to support multiple tournaments.

--- tournament.py
+++ tournament.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
 import bleach
+
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
@@ -18,7 +19,6 @@ def deleteMatches():
     cursor.execute("DELETE FROM matches")
     db.commit()
     db.close()
-    
 
 
 def deletePlayers():
@@ -31,6 +31,15 @@ def deletePlayers():
     db.close()
 
 
+def deleteTournaments():
+    """Remove all the tournament records from the database."""
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM tournaments")
+    db.commit()
+    db.close()
+
+
 def countPlayers():
     """Returns the number of players currently registered."""
     db = connect()
@@ -39,15 +48,70 @@ def countPlayers():
     count = cursor.fetchone()[0]
     db.close()
     return count
-    
+
+
+def countTournaments():
+    """Returns the number of tournaments currently registered."""
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT count(*) as num FROM tournaments")
+    count = cursor.fetchone()[0]
+    db.close()
+    return count
+
+
+def createTournament(name):
+    """Adds a tournament to the database.
+
+    Args:
+      name: the tournament's name (need not be unique).
+    """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO tournaments (name) VALUES (%s)",
+                   (bleach.clean(name), ))
+    db.commit()
+    db.close()
+
+
+def listTournaments():
+    """Returns a list of tournaments sorted by id.
+
+    Returns:
+      A list of tuples, each of which contains (id, name):
+        id: the tournament's unique id (assigned by the database)
+        name: the tournament's name (as registered)
+    """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM tournaments")
+    tournaments = cursor.fetchall()
+    db.close()
+    return tournaments
+
+
+def listPlayers():
+    """Returns a list of players sorted by id.
+
+    Returns:
+      A list of tuples, each of which contains (id, name):
+        id: the player's unique id (assigned by the database)
+        name: the player's full name (as registered)
+    """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM players")
+    players = cursor.fetchall()
+    db.close()
+    return players
 
 
 def registerPlayer(name):
-    """Adds a player to the tournament database.
-  
+    """Adds a player to the players database.
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
@@ -58,54 +122,75 @@ def registerPlayer(name):
     db.commit()
     db.close()
 
-def playerStandings():
-    """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+def addPlayerToTournamentRoster(tournament_id, player_id):
+    """Adds a previously registered player to a tournament's roster.
 
-    Returns:
-      A list of tuples, each of which contains (id, name, wins, matches):
-        id: the player's unique id (assigned by the database)
-        name: the player's full name (as registered)
-        wins: the number of matches the player has won
-        matches: the number of matches the player has played
+    Args:
+      tournament_id: the id of the tournament the player should be added to
+      played_id: the id of the player to add.
+
     """
     db = connect()
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM standings")
-    standings = [{'id'  : row[0],
-                  'name': str(row[1]),
-                  'wins': row[3],
-                  'matches': row[4],
-                  'win_percentage' : row[5]}
-                 for row in cursor.fetchall()]
+    cursor.execute("""INSERT INTO tournament_players (tournament, player)
+                   VALUES (%s, %s)""", (tournament_id, player_id))
+    db.commit()
     db.close()
-    return standings;
 
-def reportMatch(winner, loser):
+
+def playerStandings(tournament_id=-1):
+    """Returns a list of the players and their win records, sorted by wins.
+
+    The first entry in the list should be the player in first place, or a
+    player tied for first place if there is currently a tie.
+
+    Returns:
+      A list of tuples, each of which contains:
+        player_id: the player's unique id (assigned by the database)
+        player_name: the player's full name (as registered)
+        tournament_id: the tournament's unqiue id (assigned by the database)
+        tournament_name: the tournament name (assigned by the database)
+        wins: the number of matches the player has won in this tournament
+        matches: the number of matches the player has played in this tournament
+        win_percentage: The overall win percentage the player has
+    """
+    db = connect()
+    cursor = db.cursor()
+    if tournament_id == -1:
+        cursor.execute("SELECT * FROM standings")
+    else:
+        cursor.execute("SELECT * FROM standings WHERE tournament_id = %s",
+                       (tournament_id, ))
+    standings = cursor.fetchall()
+    db.close()
+    return standings
+
+
+def reportMatch(tournament, winner, loser):
     """Records the outcome of a single match between two players.
 
     Args:
+      tournament: the id of the tournament the match is for
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
     db = connect()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO matches (tournament, winner, loser) VALUES (1, %s. %s)",
-                   (bleach.clean(winner), bleach.clean(loser)))
+    cursor.execute("""INSERT INTO matches (tournament, winner, loser)
+                   VALUES (%s, %s, %s)""", (tournament, winner, loser))
     db.commit()
     db.close()
- 
- 
-def swissPairings():
+
+
+def swissPairings(tournament_id):
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -113,5 +198,13 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
+    standings = playerStandings(tournament_id)
+    pairings = []
+    for i in range(0, len(standings), 2):
+        id1 = standings[i][0]
+        name1 = standings[i][1]
+        i += 1
+        id2 = standings[i][0]
+        name2 = standings[i][1]
+        pairings.append((id1, name1, id2, name2))
+    return pairings
--- tournament_test.py
+++ tournament_test.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 #
 # Test cases for tournament.py
+# To support multiple tournaments I have made some modifications
+# to this test file. I added an additional test step and modified
+# later tests to create tournaments and register players to those
+# tournaments.
 
 from tournament import *
+
 
 def testDeleteMatches():
     deleteMatches()
@@ -12,12 +17,14 @@ def testDeleteMatches():
 def testDelete():
     deleteMatches()
     deletePlayers()
+    deleteTournaments()
     print "2. Player records can be deleted."
 
 
 def testCount():
     deleteMatches()
     deletePlayers()
+    deleteTournaments()
     c = countPlayers()
     if c == '0':
         raise TypeError(
@@ -30,6 +37,7 @@ def testCount():
 def testRegister():
     deleteMatches()
     deletePlayers()
+    deleteTournaments()
     registerPlayer("Chandra Nalaar")
     c = countPlayers()
     if c != 1:
@@ -41,6 +49,7 @@ def testRegister():
 def testRegisterCountDelete():
     deleteMatches()
     deletePlayers()
+    deleteTournaments()
     registerPlayer("Markov Chaney")
     registerPlayer("Joe Malik")
     registerPlayer("Mao Tsu-hsi")
@@ -56,73 +65,115 @@ def testRegisterCountDelete():
     print "5. Players can be registered and deleted."
 
 
+def testCreateDeleteTournament():
+    deleteMatches()
+    deletePlayers()
+    deleteTournaments()
+    createTournament("Fall Championship")
+    createTournament("Winter Championship")
+    c = countTournaments()
+    if c != 2:
+        raise ValueError(
+            "After creating 2 tournaments, countTournaments should be 2.")
+    deleteTournaments()
+    c = countTournaments()
+    if c != 0:
+        raise ValueError(
+            "After deleting, countTournaments should return zero.")
+    print "6. Tournaments can be created and deleted."
+
+
 def testStandingsBeforeMatches():
     deleteMatches()
     deletePlayers()
+    deleteTournaments()
     registerPlayer("Melpomene Murray")
     registerPlayer("Randy Schwartz")
+    createTournament("Summer Showdown")
+    tournaments = listTournaments()
+    players = listPlayers()
+    for (id, name) in players:
+        for (t_id, t_name) in tournaments:
+            addPlayerToTournamentRoster(t_id, id)
     standings = playerStandings()
     if len(standings) < 2:
-        raise ValueError("Players should appear in playerStandings even before "
-                         "they have played any matches.")
+        raise ValueError("Players should appear in playerStandings "
+                         "even before they have played any matches.")
     elif len(standings) > 2:
         raise ValueError("Only registered players should appear in standings.")
-    if len(standings[0]) != 4:
-        raise ValueError("Each playerStandings row should have four columns.")
-    [(id1, name1, wins1, matches1), (id2, name2, wins2, matches2)] = standings
+    if len(standings[0]) != 7:
+        raise ValueError("Each playerStandings row should have seven columns.")
+    [(id1, name1, t_id1, t_name1, wins1, matches1, winpercent1),
+     (id2, name2, t_id2, t_name2, wins2, matches2, winpercent2)] = standings
     if matches1 != 0 or matches2 != 0 or wins1 != 0 or wins2 != 0:
         raise ValueError(
             "Newly registered players should have no matches or wins.")
     if set([name1, name2]) != set(["Melpomene Murray", "Randy Schwartz"]):
-        raise ValueError("Registered players' names should appear in standings, "
-                         "even if they have no matches played.")
-    print "6. Newly registered players appear in the standings with no matches."
+        raise ValueError("Registered players' names should appear in "
+                         "standings, even if they have no matches played.")
+    print "7. Newly registered players appear in the standings with no matches."  # noqa
 
 
 def testReportMatches():
     deleteMatches()
     deletePlayers()
+    deleteTournaments()
     registerPlayer("Bruno Walton")
     registerPlayer("Boots O'Neal")
     registerPlayer("Cathy Burton")
     registerPlayer("Diane Grant")
+    createTournament("Winter Championship")
+    tournaments = listTournaments()
+    players = listPlayers()
+    for (t_id, t_name) in tournaments:
+        for (id, name) in players:
+            addPlayerToTournamentRoster(t_id, id)
+        standings = playerStandings()
+        [id1, id2, id3, id4] = [row[0] for row in standings]
+        reportMatch(t_id, id1, id2)
+        reportMatch(t_id, id3, id4)
     standings = playerStandings()
-    [id1, id2, id3, id4] = [row[0] for row in standings]
-    reportMatch(id1, id2)
-    reportMatch(id3, id4)
-    standings = playerStandings()
-    for (i, n, w, m) in standings:
+    for (i, n, t_i, t_n, w, m, wp) in standings:
         if m != 1:
             raise ValueError("Each player should have one match recorded.")
         if i in (id1, id3) and w != 1:
             raise ValueError("Each match winner should have one win recorded.")
         elif i in (id2, id4) and w != 0:
-            raise ValueError("Each match loser should have zero wins recorded.")
-    print "7. After a match, players have updated standings."
+            raise ValueError(
+                "Each match loser should have zero wins recorded.")
+    print "8. After a match, players have updated standings."
 
 
 def testPairings():
     deleteMatches()
     deletePlayers()
+    deleteTournaments()
     registerPlayer("Twilight Sparkle")
     registerPlayer("Fluttershy")
     registerPlayer("Applejack")
     registerPlayer("Pinkie Pie")
-    standings = playerStandings()
-    [id1, id2, id3, id4] = [row[0] for row in standings]
-    reportMatch(id1, id2)
-    reportMatch(id3, id4)
-    pairings = swissPairings()
-    if len(pairings) != 2:
-        raise ValueError(
-            "For four players, swissPairings should return two pairs.")
-    [(pid1, pname1, pid2, pname2), (pid3, pname3, pid4, pname4)] = pairings
-    correct_pairs = set([frozenset([id1, id3]), frozenset([id2, id4])])
-    actual_pairs = set([frozenset([pid1, pid2]), frozenset([pid3, pid4])])
-    if correct_pairs != actual_pairs:
-        raise ValueError(
-            "After one match, players with one win should be paired.")
-    print "8. After one match, players with one win are paired."
+    createTournament("Open Invitational")
+    createTournament("Tournament of Champions")
+    tournaments = listTournaments()
+    players = listPlayers()
+    for (t_id, t_name) in tournaments:
+        for (id, name) in players:
+            addPlayerToTournamentRoster(t_id, id)
+        standings = playerStandings(t_id)
+        [id1, id2, id3, id4] = [row[0] for row in standings]
+        reportMatch(t_id, id1, id2)
+        reportMatch(t_id, id3, id4)
+        pairings = swissPairings(t_id)
+        if len(pairings) != 2:
+            raise ValueError(
+                "For four players, swissPairings should return two pairs.")
+        [(pid1, pname1, pid2, pname2), (pid3, pname3, pid4, pname4)] = pairings
+        correct_pairs = set([frozenset([id1, id3]), frozenset([id2, id4])])
+        actual_pairs = set([frozenset([pid1, pid2]), frozenset([pid3, pid4])])
+        if correct_pairs != actual_pairs:
+            raise ValueError(
+                "After one match, players with one win should be paired.")
+    print "9. After one match, players with one win are paired."
 
 
 if __name__ == '__main__':
@@ -131,9 +182,8 @@ if __name__ == '__main__':
     testCount()
     testRegister()
     testRegisterCountDelete()
+    testCreateDeleteTournament()
     testStandingsBeforeMatches()
     testReportMatches()
     testPairings()
     print "Success!  All tests pass!"
-
-
